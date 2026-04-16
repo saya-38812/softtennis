41ca1867b3e1ced18aa9337cf22fbab99b8fdda3
saya38812 <Saya78123@gmail.com>
2026-04-16 20:14:36 +0900

    webm_mp4

--- backend/main.py
+++ backend/main.py
@@ -119,7 +119,7 @@ async def api_seed_sample():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
